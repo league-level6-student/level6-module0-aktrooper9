9a428f0f2f057e7100468b877fe6dfdc3620f5fd
league <alishahussain1229@gmail.com>
2023-04-08 11:25:07 -0700

    Mocking is weird

--- src/main/java/_04_jeopardy_api/JeopardyRunner.java
+++ src/main/java/_04_jeopardy_api/JeopardyRunner.java
@@ -11,7 +11,7 @@ public class JeopardyRunner {
         JeopardyApi jeopardyApi = new JeopardyApi();
 
         //1. Create a score variable
-int score;
+int score=0;
         //2. Add a for loop where:
         //i starts at 100,
         //continues while i <= 1000
@@ -25,21 +25,24 @@ if(i==700||i==900){
     continue;
 }
             //4. Call the getClue() method with i
-
+jeopardyApi.getClue(i);
             //5. Save the question in a String variable
-
+String question = jeopardyApi.getClue(i).getQuestion();
             //6. Save the answer in a String variable
-
+String answer = jeopardyApi.getClue(i).getAnswer();
             //7. Save the title in a String variable
             //note that this is part of the Category object
-
+String title = jeopardyApi.getClue(i).getCategory().getTitle();
             //8. Use a JOptionPane to display the question.
             //You can set the title of the JOptionPane to the question title.
-
+String input = JOptionPane.showInputDialog(null,question+" Score:"+score);
             //9. If they got the question correct, add the value of that question to their score
-
+if(input.equalsIgnoreCase(answer)){
+    score=score+i;
+}
             //10. Tell the user their final score
         }
+        JOptionPane.showMessageDialog(null,score);
     }
 
 }
